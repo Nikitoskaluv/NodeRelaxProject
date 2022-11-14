03a91dee5cf17c565d04bfe3e8ef954208c3f959
Nikitoskaluv <anastasiyak90@mail.rum>
2022-11-14 19:52:01 +0300

    Timer Save Change

--- services/timerService.js
+++ services/timerService.js
@@ -1,6 +1,11 @@
 import * as db from "../db/db.js";
 
 export function saveTimer(timer) {
+    const timerFromDB = db.getTimerById(timer.id);
+    if (timerFromDB) {
+        return db.updateTimer(timer.id, timer);
+    }
+
     return db.addTimer(timer);
 }
 export function getAllTimers() {
